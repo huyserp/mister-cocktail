37164c5608325657033d92f9910cea1e22793dfd
peter huyser <huyserp@gmail.com>
2021-01-16 17:03:32 +0100

    both cocktails and doses controllers and actions working

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,25 @@
+class DosesController < ApplicationController
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(params_dose)
+    @dose.cocktail = @cocktail
+    if @dose.save!
+      redirect_to cocktail_path(@cocktail)
+    else
+      render 'cocktails/show'
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @cocktail = @dose.cocktail
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  def params_dose
+    params.require(:dose).permit(:ingredient_id, :description)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+Ingredient.destroy_all
+puts "creating a list of ingredients"
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+
+serialized = open(url).read
+ingredients = JSON.parse(serialized)
+
+ingredients['drinks'].each do |ingredient|
+  Ingredient.create(name: ingredient['strIngredient1'])
+end
+puts "mischief managed"
+
+Cocktail.destroy_all
+puts "Creating your drink list"
+drink_list = ['Mojito', 'Bloody Mary', 'Margarita', 'Old Fashioned', 'Gin & Tonic']
+drink_list.each { |drink| Cocktail.create(name: drink)}
+puts 'mischief managed again'
