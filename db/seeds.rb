21fb2203d2e91a525893728e8ae22b9b6629b49c
peter huyser <huyserp@gmail.com>
2021-01-19 20:11:09 +0100

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'json'
 require 'open-uri'
+Cocktail.destroy_all
+puts "Creating your drink list"
+drink_list = ['Mojito', 'Bloody Mary', 'Margarita', 'Old Fashioned', 'Gin & Tonic']
+drink_list.each { |drink| Cocktail.create(name: drink)}
+puts 'mischief managed'
+
 Ingredient.destroy_all
 puts "creating a list of ingredients"
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
@@ -17,10 +23,5 @@ ingredients = JSON.parse(serialized)
 ingredients['drinks'].each do |ingredient|
   Ingredient.create(name: ingredient['strIngredient1'])
 end
-puts "mischief managed"
+puts "mischief managed again"
 
-Cocktail.destroy_all
-puts "Creating your drink list"
-drink_list = ['Mojito', 'Bloody Mary', 'Margarita', 'Old Fashioned', 'Gin & Tonic']
-drink_list.each { |drink| Cocktail.create(name: drink)}
-puts 'mischief managed again'
